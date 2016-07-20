6fa88b6c90f69e44b153ca8f8491375bacf506b9
Logan Buckley <lbuckley@opinionlab.com>
2016-07-20 16:56:17 -0400

    Add cd builtin

--- ship.py
+++ ship.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-
 import parser
 
 
@@ -15,6 +14,8 @@ def main():
         cmd = parser.parse_args(raw_input())
         if cmd[0] == 'exit':
             break
+        elif cmd[0] == 'cd':
+            os.chdir(cmd[1])
         else:
             os.spawnvp(os.P_WAIT, cmd[0], cmd)
 
